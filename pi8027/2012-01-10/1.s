5c042398ca8fa5c0c789575250a7fc3d17adcb19
Kazuhiko Sakaguchi <pi8027@gmail.com>
2012-01-10 11:37:05 +0900

    2012-01-10

---
+++ pi8027/2012-01-10/1.s
@@ -0,0 +1,14 @@
+.text
+.globl main
+main:
+    mov $0, %eax
+    mov $1, %ebx
+    mov $1, %ecx
+L1: mov %ebx, %edx
+    add %ecx, %edx
+    mov %ecx, %ebx
+    mov %edx, %ecx
+    add $1, %eax
+    cmp $10, %eax
+    jne L1
+	call stop
---
+++ pi8027/2012-01-10/2.s
@@ -0,0 +1,15 @@
+.data
+.align 4
+x:  .long 3456789012
+y:  .long 2345678901
+.text
+.globl main
+main:
+    mov $1, %eax
+    mov x, %ebx
+    mov y, %ecx
+    add %ebx, %ecx
+    jo L1
+    mov $0, %eax
+L1:
+	call stop
